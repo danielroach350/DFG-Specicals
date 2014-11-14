8fd3ded064cf6391cd8bcdc5b49e3107c641658d
Daniel Roach <danielroach@Daniels-MacBook-Pro.local>
2014-11-14 15:43:26 -0800

    Initial Commit

---
+++ DGF_Spectacles/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DGF_Spectacles
+//
+//  Created by Daniel Roach on 11/14/14.
+//  Copyright (c) 2014 Daniel Roach. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DGF_Spectacles/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DGF_Spectacles
+//
+//  Created by Daniel Roach on 11/14/14.
+//  Copyright (c) 2014 Daniel Roach. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
